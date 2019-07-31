6e6a57e2ba73aa1c50f517a8a17355e86053e832
unknown <chinasoft.jie.zhang@JUSDA.INT>
2019-07-31 17:36:50 +0800

    first commit

---
+++ src/main/java/com/example/loanapprovalspringboot/LoanApprovalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.loanapprovalspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApprovalSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApprovalSpringBootApplication.class, args);
+    }
+
+}
